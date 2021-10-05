b83327f78dc5ee0e4136ebc007c25a1c730110d6
Micha de Vries <contact@michadevries.nl>
2021-10-05 16:32:17 +0200

    Bug fix

--- pb_entry.php
+++ pb_entry.php
@@ -7,7 +7,7 @@
         public function initialize() {
             Event::listen('meta_class_generation', function($meta) {
                 return array(
-                    "title" => "Sample: " . $meta->title
+                    "title" => "Sample: " . $meta['title']
                 );
             });
         }
