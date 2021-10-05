727044dd7150cfdc5a88e6ea7998506834cbd8af
Micha de Vries <contact@michadevries.nl>
2021-10-05 15:44:15 +0200

    Fixed issue

--- pb_entry.php
+++ pb_entry.php
@@ -7,9 +7,7 @@
         public function initialize() {
             Event::listen('meta_class_generation', function($meta) {
                 return array(
-                    array(
-                        "title" => "Sample: " . $meta->title
-                    )
+                    "title" => "Sample: " . $meta->title
                 );
             });
         }
